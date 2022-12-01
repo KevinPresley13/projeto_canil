7f48a793eddab93d8dcc97ade30debe4873e59e8
Kevin Presley <kevinpresley1995@gmail.com>
2022-12-01 19:41:48 -0300

    deixando os textos dinamicos

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -3,20 +3,40 @@ import { Request, Response } from "express";
 
 
 export const home = (req: Request, res: Response)=>{
-    res.render('pages/page')
-}
+    res.render('pages/page', {
+        banner: {
+            title: 'Todos os animais',
+            background: 'allanimals.jpg'
+        }
+    })
+};
 
 export const dogs = (req: Request, res: Response)=>{
-
-}
+    res.render('pages/page', {
+        banner: {
+            title: 'Todos os cachorros',
+            background: 'banner_dog.jpg'
+        }
+    })
+};
 
 export const cats = (req: Request, res: Response)=>{
-
-}
+    res.render('pages/page', {
+        banner: {
+            title: 'Todos os gatos',
+            background: 'banner_cat.jpg'
+        }
+    })
+};
 
 export const fishs = (req: Request, res: Response)=>{
-
-}
+    res.render('pages/page', {
+        banner: {
+            title: 'Todos os peixes',
+            background: 'banner_fish.jpg'
+        }
+    })
+};
 
 
 
